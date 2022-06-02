02399266485e048a29d74d039bd0090affaac9a1
Ma-tes <msestaubr@gmail.com>
2022-06-02 20:41:49 +0200

    initial release

---
+++ TeleBufet.Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace TeleBufet.Server 
+{
+    internal class Program 
+    {
+        private static Server server;
+
+        public static void Main() 
+        {
+            Console.WriteLine("Please enter valid ip address");
+            server = new(nameof(Server), IPAddress.Parse(Console.ReadLine()));
+            Task.Run(() => server.StartServer());
+            Console.ReadLine();
+        }
+    }
+}
